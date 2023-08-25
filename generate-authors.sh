781e1357d0cb180165316bc5ae0627acdd581034
Samuel <victorsamuelpraise@gmail.com>
2023-08-25 17:46:01 +0000

    updated author session

--- generate-authors.sh
+++ generate-authors.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# Generate Authors section in Markdown format
-AUTHORS_SECTION="## Authors\n"
 git shortlog -se \
   | perl -spe 's/^\s+\d+\s+//' \
   | sed -e '/^CommitSyncScript.*$/d' \
-  | while read author; do
-      AUTHORS_SECTION="${AUTHORS_SECTION}* ${author}\n"
-    done
-
-# Update the README file
-awk -v section="$AUTHORS_SECTION" '/## Authors/ {p=1;print;print section} p && !/## Authors/{p=0}' README.md > README_temp.md
-mv README_temp.md README.md
+  > AUTHORS
